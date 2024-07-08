813ae83c93c9bc57d81aac4cd2a28294941e2a2a
kirilldonenko <kirill.donenko@gmail.com>
2024-07-08 22:56:09 +0300

    fix logo

--- src/components/ForecastMain.vue
+++ src/components/ForecastMain.vue
@@ -31,7 +31,7 @@ const addWeatherCard = () => {
 <template>
   <div class="forecast-main">
     <div class="forecast-main__header">
-      <img src="../assets/logo.png" class="forecast-main__logo" width="200" height="55" alt="Weather forecast" />
+      <img src="/weather-forecast/logo.png" class="forecast-main__logo" width="200" height="55" alt="Weather forecast" />
       <h1 class="forecast-main__title">{{$t('weather.logo')}}</h1>
       <div class="forecast-main__lang-switcher">
         <button @click="changeLang('en')">Eng</button>
