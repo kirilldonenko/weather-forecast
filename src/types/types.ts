1043ec409cf4f5b15aa757d82f3f1fa7d455d7e2
kirilldonenko <kirill.donenko@gmail.com>
2024-07-08 19:38:24 +0300

    Updates

--- src/types/types.ts
+++
@@ -1,8 +0,0 @@
-export interface ICard {
-  id: string;
-  city: string;
-  latitude: number;
-  longitude: number;
-  weather: any;
-  isFavorite: boolean;
-}
--- src/types/vue-google-autocomplete.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'vue-google-autocomplete' {
-  import { DefineComponent } from 'vue';
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
-}
